c027e323c443e99abcad012f4ba841a40f89c1ca
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-03 11:45:02 +0200

    finish

---
+++ jssnack2/README.md
@@ -0,0 +1,8 @@
+L'utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+
+--chiedo la prima parola all'utente con un promt
+--chiedo la seconda parola all'utente con un promt
+
+se la prima parola e piu lunga della seconda allora stampo prima la prima parola poi al seconda 
+altrimenti stampo prima la seconda poi la prima
---
+++ jssnack2/main.js
@@ -0,0 +1,15 @@
+// --chiedo la prima parola all'utente con un promt
+// --chiedo la seconda parola all'utente con un promt
+
+// se la prima parola e piu lunga della seconda allora stampo prima la prima parola poi al seconda 
+// altrimenti stampo prima la seconda poi la prima
+
+
+const firstWord = prompt("inserisci la prima parola");
+const secondWord = prompt("inserisci la seconda parola");
+
+if (firstWord.length>secondWord.length){
+    console.log (`${secondWord} ${firstWord}`)
+}else{
+    console.log (`${firstWord} ${secondWord}`)
+}

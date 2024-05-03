ef5a59d05121c64693ccbb2f3b4a947e29abadcd
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-03 12:41:16 +0200

    finish snack3

---
+++ jssnack3/main.js
@@ -0,0 +1,15 @@
+
+const num1 = +(prompt("Inserisci Numero 1"));
+const num2 = +(prompt("Inserisci Numero 2"));
+const num3 = +(prompt("Inserisci Numero 3"));
+const num4 = +(prompt("Inserisci Numero 4"));
+const num5 = +(prompt("Inserisci Numero 5"));
+const num6 = +(prompt("Inserisci Numero 6"));
+const num7 = +(prompt("Inserisci Numero 7"));
+const num8 = +(prompt("Inserisci Numero 8"));
+const num9 = +(prompt("Inserisci Numero 9"));
+const num10 = +(prompt("Inserisci Numero 10"));
+
+
+const somma = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+console.log (somma);
